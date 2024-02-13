4ab91fd4323326016dc44cf4e88e4ad7fd64671a
KJohnson82 <rojohnson@gmail.com>
2024-02-13 16:29:30 -0600

    Search shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-
 import tailwind from "@astrojs/tailwind";
+import orama from '@orama/plugin-astro'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +8,16 @@ export default defineConfig({
   prefetch: {
     prefetchAll: true,
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), orama({
+    empdb: {
+      pathMatcher: /^(\/corporate|\/plant)\/.+$/,
+      language: 'english',
+      contentSelectors: ['h4', 'h3']
+    },
+    retaildb: {
+      pathMatcher: /^(\/metalmart|\/servicecenter)\/.+$/,
+      language: 'english',
+      contentSelectors: ['h4', 'h3', 'span',],
+    },
+  }),],
 });
